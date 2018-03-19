e03c572a172b06630e775d675e1c01342d27b9ec
Kim, Jin <jinn.kim@sk.com>
2018-03-19 15:50:12 +0900

    Chapter 9 section 6 hashable

--- vector2d.py
+++ vector2d.py
@@ -5,8 +5,16 @@ class Vector2d:
     typecode = 'd'
 
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.__x = float(x)
+        self.__y = float(y)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
 
     def __iter__(self):
         return (i for i in (self.x, self.y))
@@ -24,6 +32,9 @@ class Vector2d:
 
     def __eq__(self, other):
         return tuple(self) == tuple(other)
+
+    def __hash__(self):
+        return hash(self.x) ^ hash(self.y)
 
     def __abs__(self):
         return math.hypot(self.x, self.y)
